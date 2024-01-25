f7622dad3018abd8a5f3a9f6227ddfea2634e398
Alan-H-H <123487644+Alan-H-H@users.noreply.github.com>
2024-01-25 01:21:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="index.css">
-    </head>
-    <body>
-        <h1>threads number:</h1>
-        <h2 id="count-el">0</h2>
-        <button id="increment-btn" onclick="increment()">INCREMENT</button>
-        <button id="save-btn" onclick="save()">SAVE</button>
-        <p id="save-el">Previous entries: </p>
-        <script src="index.js">
-         
-        </script>
-    </body>
-</html>
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
